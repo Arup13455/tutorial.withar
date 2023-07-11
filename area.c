5eb68c979342611ff308176f5f1a8bf2f35a562b
Arup13455 <137142582+Arup13455@users.noreply.github.com>
2023-07-11 09:23:55 +0530

    Create area.c

---
+++ area.c
@@ -0,0 +1,11 @@
+#include<stdio.h>//stdio.h = header file
+int main(){//main = funtcion,which returns integer type value
+  int side;//int = Datatype,side = variable
+printf("enter side: ");
+scanf("%d",&side);//%d = format specifier
+printf("Area is %d",side*side);
+return 0;
+}
+output
+enter side: 4
+  Area is 16
