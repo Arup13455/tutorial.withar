b5df760a2074231cc7556c498f65ccb7e7163519
Arup13455 <137142582+Arup13455@users.noreply.github.com>
2023-07-12 11:09:11 +0530

    Create age.c

---
+++ age.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+  int age;
+printf("Enter age: ");
+scanf("%d",age);
+if(age >= 18){
+printf("Adult");
+}else{
+printf("Not Adult\n");
+}
+return 0;
+}
+output
+Enter age: 18
+  Adult
